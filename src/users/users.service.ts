bb37f496b5856ab3ef6d3337aec651c47f303d55
Samba Diaw <sambachimerediaw@gmail.com>
2022-03-09 00:57:38 +0100

    creating and saving user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { create } from 'istanbul-reports';
+import { string } from 'yargs';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {
+    this.repo = repo;
+  }
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+}
